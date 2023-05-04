3acb7b17e604bcdcdc336ab1c8ebd7d94d574bd2
Borna92 <canicborna@gmail.com>
2023-05-04 15:00:39 -0400

    practice

---
+++ Finance page/script.js
@@ -0,0 +1,16 @@
+// const calculate = document.querySelector('.operation button');
+
+// calculate.addEventListener('click', () => {
+//     const first = document.querySelector('.search-box .first').value;
+//     const second = document.querySelector('.search-box .second').value;
+//     const operation = document.querySelector('.search-box button').value;
+//     const result = document.querySelector('.result');
+
+//     if (first === 0 || second === 0)
+//         return;
+
+//     const res = eval(`${first} ${operation} ${second}`);
+
+//     result.innerHTML = res;
+    
+// })
